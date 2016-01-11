a7f582dca591005efa678f337cc25abdeab41a0e
Matt Bowytz <mbowytz@gmail.com>
2016-01-10 20:06:03 -0500

    some views

---
+++ htdoc/home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="description" content="mattbowytz.com">
+    <meta name="author" content="Matt Bowytz">
+
+    <title>mattbowytz.com</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/cs1520.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <div class="fun">
+      <h1>WELCOME</h1>
+      <p>
+        You may have reached this page in error. There's a high probablity you are looking for my class site which can be found <a href="/cs1520.html">here</a>.
+      </p>
+    </div>
+  </body>
+</html>
