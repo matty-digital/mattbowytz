67784e2e0ee0c759e920abe16331876f70a0051f
Matt Bowytz <mbowytz@gmail.com>
2016-01-10 22:30:03 -0500

    final edits

--- htdoc/index.php
+++ htdoc/index.php
@@ -1,8 +1,3 @@
 <?php
 	require('../../vendor/autoload.php');
-
-	$app->get('/cowsay', function() use($app) {
-	  $app['monolog']->addDebug('cowsay');
-	  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
-	});
 ?>
