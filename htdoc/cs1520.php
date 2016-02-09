57da0d7556a0f0f05d6ebc26ec2fefd5555d4229
Matt Bowytz <mbowytz@gmail.com>
2016-02-08 21:10:22 -0500

    updates for project specs

--- htdoc/cs1520.php
+++ htdoc/cs1520.php
@@ -383,11 +383,21 @@
           <div class="col-lg-12 centered">
             <p class="uc red-txt bold">Class Announcements and Info</p>
             <ul class="info-bubble last-row">
+              <li class="info-date">2.8.16</li>
+              <li class="info">
+                <span class="glyphicon glyphicon-ok-circle"></span>
+                Detailed specifications for the Class Project Part 1 are now available on the site! Please let me know if you have any quesitons.
+              </li>
+            </ul>
+            <ul class="info-bubble last-row">
               <li class="info-date">2.7.16</li>
               <li class="info">
                 <span class="glyphicon glyphicon-ok-circle"></span>
-                You may have noticed that there are now a few In-Class Exercises. These can be completed in recitation between now and the end of the class.
-              </li>              
+                You may have noticed that there are now a few In-Class Exercises. These can be completed in recitation between now and the end of the class. 
+              </li>
+              <li>
+                EDIT: As long as the In-Class assignments are delivered to me by the end of the term they will be accepted as on time.
+              </li>
               <li class="info">
                 <span class="glyphicon glyphicon-ok-circle"></span>
                 All of the exercises are still a required component of the class. I just wanted to throw these out there so you can think about them ahead of time.
@@ -1186,22 +1196,136 @@
         <div class="row">
           <div class="col-lg-12 centered">
             <h1>Programming Project</h1>
-            <p class="med">Class Project: Promotional Website</p>
+            <p class="med">Class Project: PHP Website</p>
+          </div>
+          <span class="glyphicon glyphicon-star-empty block centered b-ten"></span>
+          <div class="col-lg-12 centered">
+            <p class="uc red-txt bold">Project Plan</p>
+            <ul>
+              <li>
+                Basically, this will act as a roadmap for both you and me, so we can have a clear outline of what our projects will entail. Nobody wants to program themseleves into a corner and wind up having to scrap hours of work if they can help it. You can be as verbose as you like in this, but I would like to see at least a couple sentences on each point.
+              </li>
+              <li>
+                Your plan should include the following:
+              </li>
+              <li>
+                What technology are you going to be using? (PHP for the most part, but it never hurts to outline everything you are going to use.)
+              </li>
+              <li>
+                What is your site going to be about? Self promotion? Promotion of something else? Etc. The worst that can happen is that I will suggest another route.
+              </li>
+              <li>
+                Using a frontend framework? Which one?
+              </li>
+              <li>
+                Also, anything else you think may be important to have added to the plan. We just want to cover all our bases.
+              </li>
+              <li>
+                A list of ANY questions you may have about your idea or the project in general. This is the best time to hammer out any of the smaller details.
+              </li>
+              <li>
+                OPTIONAL: Do you want to use some advanced topics? Such as MVC structure, or using PHP as a backend for some other frontend framework, etc. This will not entitle you to different or better grading. You will still be accountable for all the same requirements. I am well enough equipped to review, grade and comment on most implementations.
+              </li>
+              <li>
+                Email me this information is whatever format you like. If you would like further direction on anything, let me know. Let's have fun and demonstrate what we've learned so far!
+              </li>
+              <li>
+                NOTE: Since the class project is split up into two milestones (part one due halfway through, part two due at the end), this is really only in relation to the first part of this project. All the requirements will be the same for part two, but just implementing features based on what we will learn during the second half of the class.
+              </li>
+            </ul>
           </div>
           <span class="glyphicon glyphicon-star-empty block centered b-ten"></span>
           <div class="col-lg-12 centered">
             <p class="uc red-txt bold">Requirements</p>
-            <p>TBA</p>
+            <ul>
+              <li>
+                One page or multi page layout. Minimum of three sections/pages
+              </li>
+              <li>
+                Page Examples:
+              </li>
+              <li>
+                About, contact, resume/CV, fan page about your favorite rapper or super hero, anything is fine. Just remember though, you may want to keep it professional in the event you want to use this as your actual personal site down the road.
+              </li>
+              <li>
+                You may use a frontend framework, such as Bootstrap or Foundation. You may also use a template, all I ask is that you do not use it as it comes. Add your own layer of style on top of what is included.
+              </li>
+              <li>
+                All pages (views and includes, etc) should be written in PHP, at least for this first part of the project
+              </li>
+              <li>
+                One component/page of your site should be a contact form, I would prefer this to submit to a database, but if you want to defer the database and have it email you or write to a text file, that is ok too.
+              </li>
+              <li>
+                Programming Requirements:
+              </li>
+              <li>
+                Intelligent use of topics covered in class so far. Such as:
+              </li>
+              <li>
+                Use of variables, includes, arrays, functions, file reading etc.
+              </li>
+              <li>
+                When should I use [requirement]?
+              </li>
+              <li>
+                Ex. If you have a collection of links that have text and hrefs that need to be displayed, store them in an array and correctly loop through them where you want them to display
+              </li>
+              <li>
+                Ex. Repeating some PHP more than once in a PHP block? Store it in a variable.
+              </li>
+              <li>
+                Ex. Don't want to write your header and footer on each page for a multi page approach? Use an include file. Similarly, if you have a function that you will use over and over, that should also go into an include file.
+              </li>
+              <li>
+                There are no hard requirements for what PHP to use and when. The best approach would be to use as many of the topics covered in class (appropriately) as necessary to make the site work and prove your ability
+              </li>
+              <li>
+                If you are unsure on what to use or when/how to use it, please reach out to me or a TA
+              </li>
+              <li>
+                PLEASE DO NOT HESITATE TO ASK IF YOU NEED CLARIFICATION OR HELP WITH ANYTHING. I CANNOT STRESS THIS ENOUGH
+              </li>
+            </ul>
           </div>
           <span class="glyphicon glyphicon-star-empty block centered b-ten"></span>
           <div class="col-lg-12 centered">
             <p class="uc red-txt bold">Submission Guidelines</p>
-            <p>TBA</p>
+            <ul>
+              <li>
+                TWO methods of submission will be accepted.
+              </li>
+              <li>
+                FIRST OPTION: place everything in a GitHub repo that I can access to pull down the code. If you are not familiar with this method and would still like to submit this way, please contact me so I can help you out.
+              </li>
+              <li>
+                SECOND OPTION: zip file of all necessary files. I do not want to see the contents of the zip file to produce just a pile of files in an unorganized manner. All assets should be intelligently located in folders.
+              </li>
+              <li>
+                Basically there should be one parent folder that contains everything. Then one folder for each asset group. Ex. CSS, Images, Views etc. Feel free to do some research on the topic.
+              </li>
+            </ul>
           </div>
           <span class="glyphicon glyphicon-star-empty block centered b-ten"></span>
           <div class="col-lg-12 centered">
             <p class="uc red-txt bold">Grading Rubric</p>
-            <p>TBA</p>
+            <ul>
+              <li>
+                I'm going to be more attentive to the way you write your code for this project. In the event that it will become public facing in the future, it is important for you to have well written, clean semantic code. Myself or the TAs will be available to help you with this as well.
+              </li>
+              <li>
+                Clean, readable, sematic code, organized assets and folders, etc.: 15%
+              </li>
+              <li>
+                Intelligent implementation of requirements: 55%
+              </li>
+              <li>
+                Usability (navigation works, the site loads, assests aren't broken or unavilable, it's not too confusing to use, looks ok etc.): 25%
+              </li>
+              <li>
+                Submission guidelines are adhered to: 5%
+              </li>
+            </ul>
           </div>
         </div>
       </div>
