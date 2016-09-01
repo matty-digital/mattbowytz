b3fb1e450996bf11689920e39e0d64041a59b076
Matt Bowytz <mbowytz@gmail.com>
2016-09-01 10:07:15 -0400

    add link to lecture 1 slides

--- htdoc/includes/1520/1520materials.php
+++ htdoc/includes/1520/1520materials.php
@@ -6,6 +6,15 @@
         <p class="med">Slides and Exam/Quiz Materials</p>
       </div>
       <span class="glyphicon glyphicon-star-empty block centered b-ten"></span>
+      <div class="col-lg-12 centered">
+        <p class="uc red-txt bold">Lecture Slides</p>
+        <ul>
+          <li class="b-ten">
+            <a href="https://docs.google.com/presentation/d/1f-GPrggrd2gE5Wl_FnQiKoGfhJ6Fb1q6tBktQQ_QX1M/edit?usp=sharing">Lecture 1 - Policies and Git/GitHub</a>
+          </li>
+        </ul>
+      </div>
+      <span class="glyphicon glyphicon-star-empty block centered b-ten"></span>
     </div>
   </div>
 </section>
