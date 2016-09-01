f59add8a1112eefce57ebd25c310d395236ef9c4
Matt Bowytz <mbowytz@gmail.com>
2016-09-01 10:19:07 -0400

    134 announcement

--- htdoc/includes/0134/0134announcements.php
+++ htdoc/includes/0134/0134announcements.php
@@ -9,9 +9,8 @@
       <div class="col-lg-12 centered">
         <p class="uc red-txt bold">Class Announcements and Info</p>
         <ul class="info-bubble last-row">
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>9.1.16</li>
+          <li>Just a reminder, Project Proposal Part 1 is due before Assignment 1. Make sure to check the syllabus for assignment due dates!</li>
         </ul>
       </div>
     </div>
