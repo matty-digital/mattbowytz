9146b3847b37863b0a718042126c5728e2059a2a
Matt Bowytz <mbowytz@gmail.com>
2016-09-07 23:03:50 -0400

    second set of slides

--- htdoc/includes/1520/1520materials.php
+++ htdoc/includes/1520/1520materials.php
@@ -12,6 +12,9 @@
           <li class="b-ten">
             <a href="https://docs.google.com/presentation/d/1f-GPrggrd2gE5Wl_FnQiKoGfhJ6Fb1q6tBktQQ_QX1M/edit?usp=sharing">Lecture 1 - Policies and Git/GitHub</a>
           </li>
+          <li class="b-ten">
+            <a href="https://docs.google.com/presentation/d/1SbbgNU6tI9ox5f-f2zCL_3mLtuEA78AbSChq1bRqM7c/edit?usp=sharing">Lecture 2 - Intro to the internet</a>
+          </li>
         </ul>
       </div>
       <span class="glyphicon glyphicon-star-empty block centered b-ten"></span>
