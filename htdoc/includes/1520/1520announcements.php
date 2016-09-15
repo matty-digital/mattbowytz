d7064fad37b91383a0234a4138544065883fafb1
Matt Bowytz <mbowytz@gmail.com>
2016-09-15 17:21:39 -0400

    format announcements

--- htdoc/includes/1520/1520announcements.php
+++ htdoc/includes/1520/1520announcements.php
@@ -6,15 +6,19 @@
         <p class="med">Check back often!</p>
       </div>
       <span class="glyphicon glyphicon-star-empty block centered b-ten"></span>
-      <div class="col-lg-12 centered">
+      <div class="col-lg-12">
         <p class="uc red-txt bold">Class Announcements and Info</p>
         <ul class="info-bubble last-row">
-          <li class="b-ten">
-            QUIZ 1 - Thursday, 9.22.16 - Covers Intro to the Internet, HTML5 and CSS3
-          </li>
+          <li class="date">9.15.16</li>
           <li class="b-ten">
             Project 1 Check In 9.27.16 - You need to have at least some of the code written for this. I will also need to see it. You are not required to have it on github yet, so, showing me the code and how it's running is required. Hithub will make things easier for both of us.
             Contact me with any questions!
+          </li>
+        </ul>
+        <ul class="info-bubble last-row">
+          <li class="date">9.15.16</li>
+          <li class="b-ten">
+            QUIZ 1 - Thursday, 9.22.16 - Covers Intro to the Internet, HTML5 and CSS3
           </li>
         </ul>
       </div>
