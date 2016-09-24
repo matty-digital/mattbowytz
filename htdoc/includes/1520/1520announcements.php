eec26a5dd76cb44923fb2a64f7f70d34fe43c43c
Matt Bowytz <mbowytz@gmail.com>
2016-09-24 18:30:20 -0400

    added new announcement

--- htdoc/includes/1520/1520announcements.php
+++ htdoc/includes/1520/1520announcements.php
@@ -9,6 +9,16 @@
       <div class="col-lg-12">
         <p class="uc red-txt bold">Class Announcements and Info</p>
         <ul class="info-bubble last-row">
+          <li class="date">9.24.16</li>
+          <li class="b-ten">
+            I've added some zip files to the handouts section. These files come directly from the book, but they are still valid resources when you are just getting better with all this stuff.
+            <br>
+            You have a cool opportunity here too: Some of the formatting in the class files is a little wack. This would be a good chance to earn a bonus point or two by fixing the format to adhere to the class style guide.
+            <br>
+            Obviously you can't edit zip files, so this will be on a file by file basis. And no, you can't just get an A in thr course by re-formatting book assets.
+          </li>
+        </ul>
+        <ul class="info-bubble last-row">
           <li class="date">9.15.16</li>
           <li class="b-ten">
             Project 1 Check In 9.27.16 - You need to have at least some of the code written for this. I will also need to see it. You are not required to have it on github yet, so, showing me the code and how it's running is required. Hithub will make things easier for both of us.
