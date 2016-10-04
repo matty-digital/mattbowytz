314a87c03ff42bceb67089849605abdd0bcd0230
Matt Bowytz <mbowytz@gmail.com>
2016-10-04 16:29:25 -0400

    add links

--- htdoc/includes/0134/0134materials.php
+++ htdoc/includes/0134/0134materials.php
@@ -10,19 +10,28 @@
         <p class="uc red-txt bold">Lecture Slides</p>
         <ul>
           <li class="b-ten">
-            <a href="https://docs.google.com/presentation/d/1f-GPrggrd2gE5Wl_FnQiKoGfhJ6Fb1q6tBktQQ_QX1M/edit?usp=sharing">Lecture 1 - Policies and Git/GitHub</a>
+            <a target="_blank" href="https://docs.google.com/presentation/d/1f-GPrggrd2gE5Wl_FnQiKoGfhJ6Fb1q6tBktQQ_QX1M/edit?usp=sharing">Lecture 1 - Policies and Git/GitHub</a>
           </li>
           <li class="b-ten">
-            <a href="https://docs.google.com/presentation/d/1VLi7HAXuB6XONuKZK_kf8QXLsjq0L-4lm_0ULMUNrf8/edit?usp=sharing">Lecture 2 - Intro to Developing</a>
+            <a target="_blank" href="https://docs.google.com/presentation/d/1VLi7HAXuB6XONuKZK_kf8QXLsjq0L-4lm_0ULMUNrf8/edit?usp=sharing">Lecture 2 - Intro to Developing</a>
           </li>
           <li class="b-ten">
-            <a href="https://docs.google.com/presentation/d/1rDCCJn9pdeJY0OPh5a944CpA2-zNGP4_WVDxwSKD8fM/edit?usp=sharing">Lecture 3 - Intro to HTML5</a>
+            <a target="_blank" href="https://docs.google.com/presentation/d/1rDCCJn9pdeJY0OPh5a944CpA2-zNGP4_WVDxwSKD8fM/edit?usp=sharing">Lecture 3 - Intro to HTML5</a>
           </li>
           <li class="b-ten">
-            <a href="https://docs.google.com/presentation/d/1LlT1aNRwrHlHWZfMV1Xtt2gLGM2ZWC3pCzCFKZ5R3wQ/edit?usp=sharing">Lecture 4 - Intro to CSS</a>
+            <a target="_blank" href="https://docs.google.com/presentation/d/1LlT1aNRwrHlHWZfMV1Xtt2gLGM2ZWC3pCzCFKZ5R3wQ/edit?usp=sharing">Lecture 4 - Intro to CSS</a>
           </li>
           <li class="b-ten">
-            <a href="https://docs.google.com/presentation/d/1naM_6XH0N-HpcUzwKBnE7bxVoLwD9z1I9xCrpyBTuPA/edit?usp=sharing">Lecture 4.5 - More CSS</a>
+            <a target="_blank" href="https://docs.google.com/presentation/d/1naM_6XH0N-HpcUzwKBnE7bxVoLwD9z1I9xCrpyBTuPA/edit?usp=sharing">Lecture 4.5 - More CSS</a>
+          </li>
+          <li class="b-ten">
+            <a target="_blank" href="https://docs.google.com/presentation/d/1x4-aUMd6ghuXsjHh9nGIgtwzGvjWz8TX90kned_QlyE/edit?usp=sharing">Lecture 5 - Responsive Design</a>
+          </li>
+          <li class="b-ten">
+            <a target="_blank" href="https://docs.google.com/presentation/d/1nFMwd_UB0CQproH-c5iUkCpj4_kJ6tnPLW1sSabRidY/edit?usp=sharing">Lecture 6 - HTML Forms</a>
+          </li>
+          <li class="b-ten">
+            <a target="_blank" href="https://docs.google.com/presentation/d/1GPwX0ZKHo5QYWxux7EhzhnoQJ7l3e31X4QbXN4p8OjM/edit?usp=sharing">Lecture 7 - Website Design</a>
           </li>
         </ul>
       </div>
