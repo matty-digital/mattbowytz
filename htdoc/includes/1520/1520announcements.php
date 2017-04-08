595937b7ac26739092a45e71a6008ceac4d13ce9
Matt Bowytz <matty-digital@users.noreply.github.com>
2017-04-08 14:25:06 -0400

    Update 1520announcements.php

--- htdoc/includes/1520/1520announcements.php
+++ htdoc/includes/1520/1520announcements.php
@@ -9,6 +9,11 @@
       <div class="col-lg-12">
         <p class="uc red-txt bold">Class Announcements and Info</p>
         <ul class="info-bubble last-row">
+          <li class="date">4.8.17</li>
+          <li>Sign Up Sheets for presentations</li>
+          <li><a target="_blank" href="https://docs.google.com/spreadsheets/d/1Ufw8ZaWeaoAwECAhpkeuKVvStLRa0ufL9O2rxABkUeg/edit?usp=sharing">Sign Up!</a></li>
+        </ul>
+        <ul class="info-bubble last-row">
           <li class="date">1.23.17</li>
           <li>New office hours have been added...check the instructor section for more deets!</li>
           <li>Updated TA information has been added to the TA Info section as well!</li>
