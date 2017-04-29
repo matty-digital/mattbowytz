bb7913883471ce86c35d12dead6fb99712932995
dgmarcinek <dmarcinek@pitt.edu>
2017-04-28 23:54:00 -0400

    Update addText.js

--- ProjectPart2/JS/addText.js
+++ ProjectPart2/JS/addText.js
@@ -1,3 +1,3 @@
 function addText(id) {
-	id.innerHTML = "Your new favorite stop for sporting goods in PA, and everywhere else!";
+  id.innerHTML = "Your new favorite stop for sporting goods in PA, and everywhere else!";
 }
